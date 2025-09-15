f4d46d610a49b97317d89fc39812ed249185b874
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-09-15 23:36:46 +0530

    Add signup and signin functionality

--- routes/admin.js
+++ routes/admin.js
@@ -3,19 +3,21 @@ const adminRouter = Router();
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
+const bcrypt = require("bcrypt");
+const { z } = require("zod");
 
-const { adminModel } = require("../db");
+const { adminModel, AdminModel } = require("../db");
 
 function adminAuth() {
     const token = req.headers.token;
 
-    if(!token) {
-       return res.json({
+    if (!token) {
+        return res.json({
             message: "No token Provided"
         })
     }
 
-    try{
+    try {
         const decodedData = jwt.verify(token, process.env.JWT_ADMIN_SECRET);
 
         req.userId = decodedData.id
@@ -25,19 +27,80 @@ function adminAuth() {
         })
         console.log(error)
     }
-    
+
 }
 
-adminRouter.post("/signup", function (req, res) {
+adminRouter.post("/signup", async function (req, res) {
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        password: z.string()
+            .min(8, { message: "Password should have minimum length of 8" })
+            .max(15, "Password is too long")
+            .regex(/^(?=.*[A-Z]).{8,}$/, {
+                message:
+                    "Should Contain at least one uppercase letter and have a minimum length of 8 characters.",
+            }),
+        firstName: z.string().min(3).max(100),
+        lastName: z.string().min(3).max(100)
+    });
+
+    const parsedWithSuccess = requiredBody.safeParse(req.body);
+
+    if (!parsedWithSuccess.success) {
+        return res.json({
+            message: "Invalid Format"
+        });
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    await AdminModel.create({
+        email: email,
+        password: hashedPassword,
+        firstName: firstName,
+        lastName: lastName
+    })
+
     res.json({
-        message: "signup endpoint"
+        message: "You are signed up successfully as Admin"
     })
 });
 
-adminRouter.post("/signin", function (req, res) {
-    res.json({
-        message: "chasma endpoint"
-    })
+adminRouter.post("/signin", async function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const response = await AdminModel.findOne({
+        email: email
+    });
+
+    if (!response) {
+        return res.json({
+            message: "This Admin User does not exist"
+        })
+    }
+
+    const passwordMatched = await bcrypt.compare(password, response.password);
+
+    if (passwordMatched) {
+        const token = jwt.sign({
+            id: response._id
+        }, process.env.JWT_ADMIN_SECRET);
+
+        res.json({
+            token,
+            message: "You are logged in successfully as Admin"
+        })
+    } else {
+        res.status(403).json({
+            message: "Invalid Credentials"
+        })
+    }
 });
 
 module.exports = {
