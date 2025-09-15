a90623f8da6e01c7b8b923f0d0b4d7edbf42ef7c
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-09-15 22:39:01 +0530

    Add Course Routes

---
+++ routes/course.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+const courseRouter = Router();
+
+courseRouter.post("/purchase", function(req, res) {
+    
+});
+
+courseRouter.get("/preview", function(req, res) {
+    res.json({
+        message: "course preview endpoint"
+    })
+});
+
+module.exports = {
+    courseRouter: courseRouter
+};
